27a9ced90a66d9bb74b8014f111c5ee1e884917d
Eugenenw12 <donbarongeoffrey@gmail.com>
2025-09-01 08:44:56 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
 function App() {
-  return (
-    <h1>Hello, Liberation Frontend is working 🚀</h1>
-  );
+  return <h1>Hello, Liberation Frontend is working!</h1>;
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
