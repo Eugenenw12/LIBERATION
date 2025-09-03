574c9dc35d4c68f7509d365a3c621aac8811f213
Eugenenw12 <donbarongeoffrey@gmail.com>
2025-09-03 13:21:04 +0000

    Added Hello World component

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div>
+              <h1>
+            Hello, World! 🚀</h1>
+                    <p>My first React app is running!</p>
+                        </div>
+                          );
+                          }
+
+                          export default App;
+}
